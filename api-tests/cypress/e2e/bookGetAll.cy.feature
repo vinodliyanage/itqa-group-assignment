3326ce977e7c88cbae5f794ea5350fd44fb20882
Athilani <athilanishalinga@gmail.com>
2025-01-01 14:08:47 +0530

    Get All initial commit

--- api-tests/cypress/e2e/bookGetAll.cy.feature
+++ api-tests/cypress/e2e/bookGetAll.cy.feature
@@ -1,1 +1,10 @@
-# this is for the book get all feature.
+Feature: Books API Testing 
+As a User
+I want to test the Books API
+So that I can Verify its functionality
+
+  Scenario: Get all Books
+    Given the Books API endpoint is "/books"
+    When I send a GET request
+    Then the response status should be 200
+    And the response should contain a list of books
--- api-tests/cypress/e2e/bookGetAll.cy.mjs
+++ api-tests/cypress/e2e/bookGetAll.cy.mjs
@@ -1,1 +1,20 @@
-// TODO: add tests for getting all books.
+import {Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
+
+Given ("the Books API endpoint is {string}", (endpoint) => {
+    cy.wrap(endpoint).as("apiEndpoint");
+});
+
+When ("I send a GET request", function () {
+    cy.request("GET", this.apiEndpoint).as("apiResponse");
+});
+
+Then("the response status should be {int}", function (statusCode) {
+    cy.get("@apiResponse").its("status").should("equal", statusCode);
+});
+
+Then("the response should contain a list of books", function () {
+    cy.get("@apiResponse").its("body").should((body) => {
+        expect(body).to.be.an("array");//Ensure the response is an array
+        expect(body.length).to.be.greaterThan(0);//Ensure the array is not empty
+    });
+});
