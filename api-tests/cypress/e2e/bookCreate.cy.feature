ac7cbbe590c80a5329adc50e92ff79698683545f
Vinod Liyanage <vinodsliyanage@gmail.com>
2025-01-01 22:05:39 +0530

    implement the tests.

--- api-tests/cypress/e2e/bookCreate.cy.feature
+++ api-tests/cypress/e2e/bookCreate.cy.feature
@@ -1,29 +1,20 @@
 Feature: Book Creation
 
-  # Scenario 1: Successfully create a book with valid title and author
-  Scenario: Successfully create a book with valid title and author
-    Given I make a POST request to "/api/books" with the following body:
-      | title      | author    |
-      | Book Title | Author A  |
+  Scenario: Regular user can successfully create a book
+    Given I am authenticated as "user" with password "password"
+    When I make a POST request to "/api/books" with the following body:
+      | title   | author   |
+      | title 1 | author 1 |
     Then the response status code should be 201
-    And the response body should contain "id" with a value that is a number
-    And the response body should contain "title" with value "Book Title"
-    And the response body should contain "author" with value "Author A"
+    And the response body should contain "title" with value "title 1"
+    And the response body should contain "author" with value "author 1"
 
-  # Scenario 2: Fail to create a book with missing title
-  Scenario: Fail to create a book with missing title
-    Given I make a POST request to "/api/books" with the following body:
-      | author    |
-      | Author B  |
-    Then the response status code should be 400
-    And the response body should contain an error message
-
-  # Scenario 3: Fail to create a book with missing author
-  Scenario: Fail to create a book with missing author
-    Given I make a POST request to "/api/books" with the following body:
-      | title     |
-      | Book Title|
-    Then the response status code should be 400
-    And the response body should contain an error message
-
-  
+  Scenario: Regular user can successfully create a book with optional id field
+    Given I am authenticated as "user" with password "password"
+    When I make a POST request to "/api/books" with the following body:
+      | id   | title   | author   |
+      | 1000 | title 2 | author 2 |
+    Then the response status code should be 201
+    And the response body should contain "id" with value "1000"
+    And the response body should contain "title" with value "title 2"
+    And the response body should contain "author" with value "author 2"
--- api-tests/cypress/e2e/bookCreate.cy.mjs
+++ api-tests/cypress/e2e/bookCreate.cy.mjs
@@ -1,45 +1,31 @@
-import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-function postBook(data) {
-  return cy.request({
-    method: "POST",
-    url: "/api/books",
-    body: data,
-    headers: {
-      Authorization: "Basic YWRtaW46cGFzc3dvcmQ=",
-      "Content-Type": "application/json",
-    },
+beforeEach(() => {
+  cy.wrap(null).as("response");
+  cy.wrap("").as("authToken");
+});
+
+Given("I am authenticated as {string} with password {string}", (username, password) => {
+  cy.getAuthToken(username, password).as("authToken");
+});
+
+When("I make a POST request to {string} with the following body:", (endpoint, dataTable) => {
+  const rawTable = dataTable.rawTable;
+
+  const headers = rawTable[0];
+  const row = rawTable[1];
+
+  const requestData = Object.fromEntries(headers.map((key, index) => [key, row[index]]));
+
+  cy.get("@authToken").then((token) => {
+    cy.createBook(requestData, endpoint, token).as("response");
   });
-}
-
-// TODO: test authentication
-
-// Step 1: Making a POST request with valid data
-Given("I make a POST request to {string} with the following body:", function (url, dataTable) {
-  const data = dataTable.hashes()[0]; 
-  postBook(data).then((res) => {
-    this.response = res; 
-  });
 });
 
-// Step 2: Verifying the response status code is 201
-Then("the response status code should be 201", function () {
-  cy.wrap(this.response.status).should("equal", 201);
+Then("the response status code should be {int}", (statusCode) => {
+  cy.get("@response").its("status").should("eq", statusCode);
 });
 
-// Step 3: Verifying the response body contains "id" with a number
-Then('the response body should contain "id" with a value that is a number', function () {
-  cy.wrap(this.response.body.id).should("be.a", "number");
+Then("the response body should contain {string} with value {string}", (field, value) => {
+  cy.get("@response").its("body").should("have.property", field).and("eq", value);
 });
-
-// Step 4: Verifying the response body contains the correct title
-Then('the response body should contain "title" with value {string}', function (expectedTitle) {
-  cy.wrap(this.response.body.title).should("equal", expectedTitle);
-});
-
-// Step 5: Verifying the response body contains the correct author
-Then('the response body should contain "author" with value {string}', function (expectedAuthor) {
-  cy.wrap(this.response.body.author).should("equal", expectedAuthor);
-});
-
-// TODO: Add more steps to test the other fields in the response body
