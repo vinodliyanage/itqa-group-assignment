ec2ebd672bf5332c4c48a0fef90d86b024c76dea
Sandarekha-Gunasiri <sandarekhagunasiri@gmail.com>
2025-01-02 15:37:42 -0800

    Implemented book update tests

--- api-tests/cypress/e2e/bookUpdate.cy.feature
+++ api-tests/cypress/e2e/bookUpdate.cy.feature
@@ -1,1 +1,37 @@
-# this is for book update feature
+Feature: Update Book API - Validate Response Codes and Data
+
+  Scenario: Fail when updating a book with an unexpected parameter
+    Given I have a book with ID 1
+    When I send a PUT request to update the book with an unexpected parameter
+    Then the response status should be 400
+    And the response body should contain "Invalid parameters"
+
+  Scenario: Fail when updating a book with an invalid ID
+    Given the book with ID 999 does not exist
+    When I send a PUT request with valid data
+    Then the response status should be 404
+    And the response body should contain "Book not found"
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- api-tests/cypress/e2e/bookUpdate.cy.mjs
+++ api-tests/cypress/e2e/bookUpdate.cy.mjs
@@ -1,1 +1,72 @@
-// TODO: add tests for updating a book
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+const baseUrl = "http://localhost:7081/api/books";
+
+Given("I have a book with ID {int}", (id) => {
+  cy.wrap(id).as("bookId");
+});
+
+When(
+  "I send a PUT request to update the book with an unexpected parameter",
+  function () {
+    const bookId = this.bookId;
+    const invalidUpdateData = {
+      id: bookId,
+      title: "Updated Book Title",
+      author: "Updated Author",
+      unexpectedParam: "InvalidParam",
+    };
+
+    cy.request({
+      method: "PUT",
+      url: `${baseUrl}/${bookId}`,
+      auth: {
+        username: "admin",
+        password: "password",
+      },
+      body: invalidUpdateData,
+      failOnStatusCode: false,
+    }).as("response");
+  }
+);
+
+When("I send a PUT request with valid data", function () {
+  const invalidBookId = 999;
+  const validUpdateData = {
+    title: "Valid Book Title",
+    author: "Valid Author",
+  };
+
+  cy.request({
+    method: "PUT",
+    url: `${baseUrl}/${invalidBookId}`,
+    auth: {
+      username: "admin",
+      password: "password",
+    },
+    body: validUpdateData,
+    failOnStatusCode: false,
+  }).as("response");
+});
+
+Then("the response status should be {int}", function (statusCode) {
+  cy.get("@response").its("status").should("eq", statusCode);
+});
+
+Then("the response body should contain {string}", function (message) {
+  cy.get("@response").its("body").should("contain", message);
+});
+
+Given("the book with ID {int} does not exist", function (bookId) {
+  cy.request({
+    method: "GET",
+    url: `${baseUrl}/${bookId}`,
+    auth: {
+      username: "admin",
+      password: "password",
+    },
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).to.eq(404); 
+  });
+});
