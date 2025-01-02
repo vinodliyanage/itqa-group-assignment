8eaa711d609cc700c24fbf2070e58f1f980708ee
shalindileesha <shalindileesha@gmail.com>
2025-01-02 10:57:03 +0530

    my 2 APIs are done

--- api-tests/cypress/e2e/bookUpdate.cy.feature
+++ api-tests/cypress/e2e/bookUpdate.cy.feature
@@ -1,1 +1,23 @@
-# this is for book update feature
+Feature: Update Book API
+
+  Scenario: Successfully update a book
+    Given the application is running
+    And a book with ID "1" exists
+    When I send a PUT request to "/api/books/1" with the following data:
+      | id  | title          | author           |
+      | 1   | Updated Title  | Updated Author   |
+    Then the response status code should be 200
+    And the response body should contain "title" with value "Updated Title"
+    And the response body should contain "author" with value "Updated Author"
+
+  Scenario: Update a book with missing required fields
+    Given the application is running
+    And a book with ID "2" exists
+    When I send a PUT request to "/api/books/2" with the following data:
+      | id  | title | author           |
+      | 2   |       | Updated Author   |
+    Then the response status code should be 400
+    And the response body should indicate invalid input
+
+
+
--- api-tests/cypress/e2e/bookUpdate.cy.mjs
+++ api-tests/cypress/e2e/bookUpdate.cy.mjs
@@ -1,1 +1,78 @@
-// TODO: add tests for updating a book
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const feature = loadFeature('./features/book-management.feature');
+const baseUrl = '/api/books';
+
+defineFeature(feature, (test) => {
+  test('Get all books', ({ given, when, then, and }) => {
+    given('the application is running', function () {
+      cy.request({
+        method: 'GET',
+        url: baseUrl,
+        headers: {
+          Authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+        },
+      }).then((response) => {
+        expect(response.status).to.be.oneOf([200, 204]);
+      });
+    });
+
+    when('I send a GET request to "/api/books"', function () {
+      cy.request({
+        method: 'GET',
+        url: baseUrl,
+        headers: {
+          Authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+        },
+      }).then((response) => {
+        this.response = response;
+      });
+    });
+
+    then('the response status code should be 200', function () {
+      expect(this.response.status).to.equal(200);
+    });
+
+    and('the response body should be an array of books', function () {
+      expect(this.response.body).to.be.an('array');
+    });
+  });
+
+  test('Create a new book', ({ given, when, then, and }) => {
+    given('the application is running', function () {
+      cy.request({
+        method: 'GET',
+        url: baseUrl,
+        headers: {
+          Authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+        },
+      }).then((response) => {
+        expect(response.status).to.be.oneOf([200, 204]);
+      });
+    });
+
+    when('I send a POST request to "/api/books" with the following data:', function (dataTable) {
+      const data = dataTable.hashes()[0];
+      cy.request({
+        method: 'POST',
+        url: baseUrl,
+        headers: {
+          Authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+          'Content-Type': 'application/json',
+        },
+        body: data,
+      }).then((response) => {
+        this.response = response;
+      });
+    });
+
+    then('the response status code should be 201', function () {
+      expect(this.response.status).to.equal(201);
+    });
+
+    and('the response body should indicate the book was created', function () {
+      expect(this.response.body).to.have.property('id');
+      expect(this.response.body).to.have.property('title');
+      expect(this.response.body).to.have.property('author');
+    });
+  });
+});
